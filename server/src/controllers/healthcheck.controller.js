21eb0936ae7d99118f95107dca53c6ca5fa8d7de
HRUTHIK <hruthik9980819630@gmail.com>
2024-07-27 20:52:00 +0530

    fix typo in healthcheck controller

--- server/src/controllers/healthcheck.controller.js
+++ server/src/controllers/healthcheck.controller.js
@@ -1,5 +1,5 @@
 import asyncHandler from "../utils/asyncHandler.js";
-import ApiResponse from "../utils/ApiResponse.js";
+import ApiResponse from "../utils/apiResponse.js";
 
 const healthcheck = asyncHandler(async (_, res) => {
     return res
